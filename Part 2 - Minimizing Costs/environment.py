8206e8a30dd7f61b1994d4eb595a0fb4204b4ff7
Juan Gabriel Gomila <juangabriel@frogames.es>
2020-08-01 09:49:53 +0200

    Update environment.py

--- Part 2 - Minimizing Costs/environment.py
+++ Part 2 - Minimizing Costs/environment.py
@@ -6,7 +6,7 @@ import numpy as np
 
 # CONSTRUIR EL ENTORNO EN UNA CLASE
 
-class Environtment(object):
+class Environment(object):
     
     # INTRODUCIR E INICIALIZAR LOS PARÁMETROS Y VARIABLES DEL ENTORNO
     def __init__(self, optimal_temperature = (18.0, 24.0), initial_month = 0, initial_number_users = 10, initial_rate_data = 60):
